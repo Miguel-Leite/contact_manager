4aecff4b477ed4000e28213112d0fa0b11d11ba4
Miguel-Leite <miguelleite200leite@gmail.com>
2022-09-15 20:27:39 +0100

    import validation

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
@@ -17,6 +18,30 @@ class ContactController extends Controller
     public function store(Request $request)
     {
         try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|max:50',
+                'email' => 'required|unique:users,email',
+                'contact' => 'required|unique:contacts,contact',
+            ], [
+                'name' => [
+                'required' => 'Campo nome é obrigatŕio.',
+                'max' => 'Nome deve ter no minimo 50 caracteres.', ],
+                'email' => [
+                    'required' => 'Campo e-mail é obrigatŕio.',
+                    'unique' => 'Este e-mail já esta registrado no sistema.',
+                ],
+                'contact' => [
+                    'required' => 'Campo e-mail é obrigatŕio.',
+                    'unique' => 'Este contacto já esta registrado no sistema.',
+                ],
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'success' => true,
+                    'message' => $validator->errors(),
+                ], 201);
+            }
             $this->instance->create($request->all());
 
             return response()->json([
@@ -35,6 +60,27 @@ class ContactController extends Controller
     {
         try {
             if ($contact = $this->instance->find($id)) {
+                $validator = Validator::make($request->all(), [
+                    'name' => 'required|max:50',
+                    'email' => 'required',
+                    'contact' => 'required',
+                ], [
+                    'name' => [
+                    'required' => 'Campo nome é obrigatŕio.',
+                    'max' => 'Nome deve ter no minimo 50 caracteres.', ],
+                    'email' => [
+                        'required' => 'Campo e-mail é obrigatŕio.',
+                    ],
+                    'contact' => [
+                        'required' => 'Campo e-mail é obrigatŕio.',
+                    ],
+                ]);
+                if ($validator->fails()) {
+                    return response()->json([
+                        'success' => true,
+                        'message' => $validator->errors(),
+                    ], 201);
+                }
                 $data = $request->all();
                 $contact->update($data);
 
