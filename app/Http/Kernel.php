f827d7bcc080957186bf4ea35a0a560d05e01cfd
Miguel-Leite <miguelleite200leite@gmail.com>
2022-09-15 15:24:47 +0100

    add midleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'authContact' => \App\Http\Middleware\AuthContact::class,
+        'authContactApi' => \App\Http\Middleware\AuthContactApi::class,
     ];
 }
